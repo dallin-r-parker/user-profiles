a1b2a34040150c6f37f960d5df19f39fe1ef2bc4
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-13 17:14:28 -0600

    finished project

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,11 +1,18 @@
+
+
+
+
 angular.module('userProfiles')
 .controller('mainCtrl', function( $scope, friendService, $location ) {
 
 	$scope.login = function( user ) {
+
 		friendService.login(user).then(function( response ) {
+
 			if (response.data.userFound) {
 				$location.path('/profile');
-			} else {
+			}
+			else {
 				alert('user not found');
 			}
 		});
